f798b7bfb7d0d67f4d6214dcf6ad7f8e414e6411
treece <tyler.l.reece2.mil@army.mil>
2022-08-16 14:25:14 -0700

    commit

--- init_repo.sh
+++ init_repo.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+shopt -s extglob
+
+# "make clean" option
+if [ "$1" = "clean" ]
+then
+	rm -rfv !("$0") > /dev/null 2>&1
+	rm .gitignore
+	exit 1
+fi
+
 # Variables
 PROJ_NAME="testProj"
 BUILD_SCRIPT="build.sh"
@@ -9,6 +19,7 @@ TEST_DIR="tests"
 REF_DIR="references"
 SUBDIRS=($SOURCE_DIR $INCLUDE_DIR $TEST_DIR $REF_DIR)
 CMAKEFILE="CMakeLists.txt"
+PLACEHOLDER_FILES=("$SOURCE_DIR/$PROJ_NAME.c" "$SOURCE_DIR/file1.c" "$SOURCE_DIR/file2.c" "$INCLUDE_DIR/file1.h" "$INCLUDE_DIR/file2.h")
 
 # Create base README
 echo $PROJ_NAME >> "README.md"
@@ -44,7 +55,18 @@ echo -e '\t\tset(CMAKE_C_COMPILER gcc)' >> $CMAKEFILE
 echo -e '\t\tset(CMAKE_C_FLAGS "-g -Wall -pedantic")' >> $CMAKEFILE
 echo -e '\tendif()' >> $CMAKEFILE
 
+
+# Add structure for files
+# Note: This will require by-hand editing of filenames depending on project
+echo "add_executable($PROJ_NAME ${PLACEHOLDER_FILES[@]}" >> $CMAKEFILE
 echo >> $CMAKEFILE
+
+# Add Threading package
+echo "set(THREADS_PREFER_PTHREAD_FLAG ON)" >> $CMAKEFILE
+echo "find_package(Threads REQUIRED)" >> $CMAKEFILE
+echo "target_link_libraries($PROJ_NAME PRIVATE Threads::Threads" >> $CMAKEFILE
+
+# Add Address Sanitizer
 echo >> $CMAKEFILE
 echo "target_compile_options($PROJ_NAME PRIVATE -fsanitize=address)" >> $CMAKEFILE
 echo "target_link_options($PROJ_NAME PRIVATE -fsanitize=address)" >> $CMAKEFILE
